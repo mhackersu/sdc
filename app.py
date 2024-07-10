7bbfaba24b015ebee30717c0aec1e0a966c4f8f2
Mike Hacker <mhackersu@gmail.com>
2024-07-10 18:30:01 -0500

    wrap up

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import requests
 
 def main():
     currency = os.getenv('CURRENCY')
-    threshold = os.getenv('THRESHOLD')
-
-    getcontext().prec = 6
+    threshold= os.getenv('THRESHOLD')
+    # getcontext().prec = 6
     
-    def sdevCalc(currency, threshold):
-        sdev: float = 0
+    def sdevCalc(currency):
+        sd = 0
         candleLength: str = '/1day'
         candleUrl: str = 'https://api.gemini.com/v2/candles/'
         url = f'{candleUrl}{currency}{candleLength}'
@@ -29,56 +28,58 @@ def main():
             for c in d:
                 currency = c[4]
                 sum += c[4]
-                print(f'Closing price: {currency} Sum: {sum}')
+                # DEBUG
+                # print(f'Closing price: {currency} Sum: {sum}')
 
             e = len(d)
-            print(f'Total closing prices: {e}')
+            # DEBUG
+            # print(f'Total closing prices: {e}')
 
             am = sum / e
-            print(f'Arithmetic mean: {am}')
+            # DEBUG
+            # print(f'Arithmetic mean: {am}')
 
             sdsum = 0
             for c in d:
-                currency = c[4]
+                cp = c[4]
                 # Deviation = Closing price - Arithmetic mean
-                d = currency - am
+                d = cp - am
                 sdsum += d
             
-            sd: float = sdsum / am
-            print(f'Standard deviation: {sd}')
+            sdc = sdsum / am
+            sd = Decimal(sdc)
+            # DEBUG
+            # print(f'Standard deviation: {sd}')
         
         else:
             print(f'Failed to fetch data: {res.status_code}')
 
         return sd
     
-    t = float(threshold)
-    sdc = sdevCalc(currency, threshold)
-
-    print(f'SD: {sdc}')
-    print(type(sdc))
+    # Calculate Standard Deviation for Given Currency
+    sdc = sdevCalc(currency)
+    # Absolute value of sd
+    asd = abs(sdc)
+    # Quantize Decimal // TODO
+    # sdval = Decimal(sdc).quantize(Decimal('0.000001'), rounding=ROUND_UP)
+    t = Decimal(threshold)
+    print(f'Currency pair: {currency}')
     print(f'Threshold: {t}')
-    print(type(t))
+    # print(type(t))
+    print(f'Standard deviation: {asd}')
+    # print(type(sdc))
 
-    print(sdc > t)
+    # TODO Conditional statements not working correctly
+    # print(asd<t)
 
-    # if sdc > t:
-    #     sdev = True
-    # else:
-    #     sdev = False
+    # TODO Print JSON
+    # data = 'test.json'
+    # with open(data, 'r') as f:
+    #     d = json.load(f)
 
-    # Print
-    print(f'currency pair: {currency}')
-    print(f'sdev threshold: {threshold}')
-    # print(f'Exceeds Threshold: {sdev}')
-
-    data = 'test.json'
-    with open(data, 'r') as f:
-        d = json.load(f)
-
-    for r in d:
-        record = json.dumps(r)
-        print(record)
+    # for r in d:
+    #     record = json.dumps(r)
+    #     print(record)
 
 if __name__ == '__main__':
     main()
